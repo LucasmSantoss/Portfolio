9bea0c028d5f25c5f9678056464c02f22196abb8
unknown <lucasmsantoss95@hotmail.com>
2023-09-25 22:52:11 -0300

    fix

--- portfolio/src/Pages/Contact/Contact.jsx
+++ portfolio/src/Pages/Contact/Contact.jsx
@@ -1,17 +1,39 @@
-import React, { useState } from "react";
-import emailjs from "emailjs-com";
-import Swal from "sweetalert2";
-
+import React, { useRef, useState } from 'react';
+import './contact.css';
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
 
 function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const form = useRef();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(true);
 
-  const handleSubmit = (e) => {
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    const inputValue = e.target.value;
+    setEmail(inputValue);
+
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    setIsValidEmail(emailRegex.test(inputValue));
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const sendEmail = (e) => {
     e.preventDefault();
 
-   
+    if (!isValidEmail) {
+      Swal.fire('Error', 'Please enter a valid email address.', 'error');
+      return;
+    }
+
     const {
       REACT_APP_EMAILJS_SERVICE_ID: serviceID,
       REACT_APP_EMAILJS_TEMPLATE_ID: templateID,
@@ -19,99 +41,50 @@ function Contact() {
     } = process.env;
 
     emailjs
-      .sendForm(serviceID, templateID, e.target, userID)
-      .then((result) => {
-        console.log(result.text);
-        setName("");
-        setEmail("");
-        setMessage("");
-        mostrarAlerta();
-      })
-      .catch((error) => {
-        console.log(error.text);
-      });
-  };
+      .sendForm(serviceID, templateID, form.current, userID)
+      .then(
+        (result) => {
+          console.log(result.text);
+          Swal.fire('Success', 'Message sent successfully!', 'success');
+        },
+        (error) => {
+          console.log(error.text);
+          Swal.fire('Error', 'Message could not be sent. Please try again later.', 'error'); 
+        }
+      );
 
-  const mostrarAlerta = () => {
-    Swal.fire({
-      title: "Se enviará el correo a Lucas Matias Santos",
-      text: "¿Deseas continuar?",
-      icon: "question",
-      showCancelButton: true,
-      confirmButtonText: "Aceptar",
-      cancelButtonText: "Cancelar",
-    }).then((result) => {
-      if (result.isConfirmed) {
-      
-      }
-    });
+    setName('');
+    setEmail('');
+    setMessage('');
   };
 
   return (
-    <div className="pt-12 p-4 text-white body animate-slide-in-left">
-      
-  <h2 className=" text-center text-2xl md:text-5xl p-4">
-  You can contact me from here!
-  </h2>
-
-  <form
-    className="flex flex-col items-center gap-4 p-4 rounded-md"
-    onSubmit={handleSubmit}
-  >
-    <div className="grid grid-cols-1 gap-4">
-      <label className="flex flex-col gap-1">
-        <span className="text-lg rounded-md shadow-2xl underline underline-offset-41"  style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 1.5)' }}>Name:</span>
-        <input
-          className={`text-black w-full md:w-96 h-8 p-2  ${
-            name ? "bg-white" : "bg-transparent"
-          } border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500`}
-          type="text"
-          name='from_name'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </label>
-
-      <label className="flex flex-col gap-1">
-        <span className="text-lg rounded-md shadow-2xl underline underline-offset-4" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 1.5)' }}>Mail:</span>
-        <input
-          className={`text-black w-full md:w-96 h-8 p-2  ${
-            email ? "bg-white" : "bg-transparent"
-          } border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500`}
-          type="email"
-          name='user_email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-
-      <label className="flex flex-col gap-1">
-        <span className="text-lg rounded-md shadow-2xl underline underline-offset-4" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 1.5)' }}>Message:</span>
-        <textarea
-          className={`text-black w-full md:w-96 p-2 ${
-            message ? "bg-white" : "bg-transparent"
-          } border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500`}
-          style={{ height: "10rem" }}
-          value={message}
-          name='message'
-          onChange={(e) => setMessage(e.target.value)}
-          required
-        />
-      </label>
+    <div className='body flex justify-center '>
+      <div className='box'>
+        <h2>Contact me</h2>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className='user-box'>
+            <input type='text' name='user_name' required value={name} onChange={handleNameChange} />
+            <label>Name</label>
+          </div>
+          <div className='user-box'>
+            <input type='text' name='user_email' required value={email} onChange={handleEmailChange} className={!isValidEmail ? 'invalid-email' : ''} />
+            <label>Email</label>
+          </div>
+          <div className='text-area'>
+            <label>Message</label>
+            <textarea name='message' value={message} onChange={handleMessageChange} required />
+          </div>
+          <button className='btn-contact' type='submit'>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Send
+          </button>
+        </form>
+      </div>
     </div>
-
-    <button
-      className="px-4 py-2 md:px-6 md:py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-500"
-      type="submit"
-    >
-      Submit
-    </button>
-  </form>
-     
-</div>
-
   );
 }
 
